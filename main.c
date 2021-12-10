8d9626327014bfcb19b5229e6bf08f200627da8e
Austin Blackmon <jblack7@nmsu.edu>
2021-12-10 13:25:58 -0700

    initial main.c
    also testing TODO to Issue workflow

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// TODO create header file
+// create a .h header file to contain our functions
+// labels: optimization
+// assignees: austinblackmon, theautomelon
+
+int main(){
+
+    
+    printf("Hello World!");
+
+    return 0;
+}//end main
