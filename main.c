8ac0842712c32b63d19926c2df1c3ed5b92dd30b
theautomelon <grimmy546@gmail.com>
2021-12-10 19:57:13 -0600

    playin around with C

--- main.c
+++ main.c
@@ -4,10 +4,25 @@
 // labels: optimization
 // assignees: austinblackmon, theautomelon
 
-int main(){
+//functions
+int adder(int x, int y){
+    return x+y;
+}
 
-    
-    printf("Hello World!");
+int main(){
+    int num1;
+    int num2;
+    int sum;
+
+    printf("Enter a number:");
+    scanf("%d", &num1);
+
+    printf("Enter another number:");
+    scanf("%d", &num2);
+
+    sum = adder(num1,num2);
+
+    printf("The sum of your numbers is %d", sum);
 
     return 0;
 }//end main
