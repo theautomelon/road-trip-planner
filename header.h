347ff5f36ddacd78a74144cf1446c70a413294d5
Austin Blackmon <jblack7@nmsu.edu>
2021-12-10 19:16:17 -0700

    created Point structure

--- header.h
+++ header.h
@@ -1,4 +1,12 @@
 //functions
 int adder(int x, int y){
     return x+y;
-}
+}//end adder
+
+struct Point{
+    float longitude;
+    char longDir;
+    float latitude;
+    char latDir;
+
+};//end Point
